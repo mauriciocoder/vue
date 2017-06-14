5610e5dde4708a45e8b812d2334babcf89be92ed
mauriciocoder <mauricio.coder@outlook.com>
2017-06-14 11:30:49 -0300

    Lesson 17- another webpack test project

---
+++ vue-loader/lesson17/src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>The counter is always {{counter}}</h3>
+    <button @click="calculate">Calculate</button>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'counter',
+
+    data() {
+      return { counter: 0 }
+    },
+
+    methods: {
+      calculate() {
+        this.counter += 11
+      }
+    }
+  }
+</script>
+<style></style>
