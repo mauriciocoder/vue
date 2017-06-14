06a67e956473a4337a0dfb5760bd06157fdfb105
mauriciocoder <mauricio.coder@outlook.com>
2017-06-14 10:41:29 -0300

    Lesson17 - First vue project using webpack and vue-loader check reference for on http://vue-loader.vuejs.org/en/start/setup.html

---
+++ vue-loader/hello-vue/src/App.vue
@@ -0,0 +1,22 @@
+<style>
+</style>
+
+<template>
+<div id="app">
+  <message>Hello Vue Loader!!!</message>
+  <message>BIRL!!!</message>
+</div>
+</template>
+
+<script>
+import Message from './components/Message.vue'
+export default {
+    name: 'app',
+    components: { Message },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
